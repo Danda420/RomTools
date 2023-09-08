a84d1c861c3f0ffcff162110936fd58c6f9325ec
Danda420 <dandagaharu77@gmail.com>
2023-09-08 18:27:21 +0700

    add custom scripts

---
+++ scripts/custom_scripts/placeholder.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+### IMPORT FUNCTIONS AND VARIABLES FROM TOOLS ###
+source ./scripts/functions
+#################################################
+clear
+#################################################
+
+### write script here ###
+
+echo hello...
+echo $PWD
+
+#################################################
+restart_tool
+exit
+#################################################
